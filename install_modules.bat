ec4b06f3e31b44cd3a7715e7492b807eba3f382f
Chandrakant Bhalekar <chandrakanb@kpit.com>
2025-05-18 02:35:55 +0530

    Added Batch File to Install Required Modules

---
+++ install_modules.bat
@@ -0,0 +1,10 @@
+python.exe -m ensurepip --upgrade
+python.exe -m pip install --upgrade pip
+python.exe -m pip install beautifulsoup4
+python.exe -m pip install pandas
+python.exe -m pip install openpyxl
+python.exe -m pip install requests
+python.exe -m pip install selenium
+python.exe -m pip install Pillow
+python.exe -m pip install pyarrow         
+python.exe -m pip install --upgrade selenium
